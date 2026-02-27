58535683a234ea05f61f0afa69aab31058a6f860
Muhammad Sobri Maulana <sobri3195@gmail.com>
2026-02-28 01:38:42 +0700

    Fix service worker stale asset caching on deploy

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {
+    navigator.serviceWorker.register('/sw.js?v=3').then((registration) => registration.update()).catch(() => {
       // noop
     });
   });
